8abcfed00e45b6944a1e5130f8e579ce131e4179
Adomas Gaudiesius <adomas.gaudi@gmail.com>
2022-09-15 22:04:11 +0300

    feat: fix contain

--- src/components/Contain.vue
+++ src/components/Contain.vue
@@ -1,25 +1,63 @@
 <script setup>
 const props = defineProps({
-  pad: String,
   classIn: String,
   classOut: String,
+  type: String,
 });
+// export const H800p3 = base(Contain,
+const sds = {
+  classOut: "min-h-[800px]",
+  pad: "",
+  classIn: "  ",
+};
+
+// const makeComp = () => {
+//   return `(
+//     <div>
+//       <h1>Hello world</h1>
+//       <div>{{ this.msg }}</div>
+//     </div>
+//   )`
+// };
+// }
+// const HelloWorld = Vue.component("hello-world", {
+//   props: ["msg"],
+//   template: `<div>
+//     <h1>Hello world</h1>
+//     <div>{{ this.msg }}</div>
+//   </div>`,
+// });
+// export const H0p3 = base(Contain, {
+// classOut: "", pad: "px-2 md:px-[%]", classIn: " flex ", });
+// export const
+// Brilliant = base(Contain, { classOut: "", pad: "px-2 sm:px-[15%] md:px-[20%]
+// lg:px-[22%] xl:px-[24%]", classIn: " flex flex-wrap ", });
+// export const ContainX
+// = { H800p3, H0p3, Brilliant, }
+// export default {
+//   components: {
+//     HelloWorld,
+//   },
+// };
 </script>
 
 <template>
-  <main :class="`flex justify-center ${classOut ?? ' '}`">
-    <div :class="'container xl:w-[1124]'">
-      <div :class="pad ?? ' '">
-        <div :class="classIn ?? ' '"><slot /></div>
+  <main
+    :class="`flex justify-center min-h-[800px] px-2 md:px-[3%] ${
+      classOut ?? ' '
+    }`"
+  >
+    <div :class="'container xl:max-w-[1124] '">
+      <div :class="classIn ?? 'flex flex-col bg-gray-900 text-white p-4'">
+        <slot />{{ classOut }}
       </div>
     </div>
   </main>
 </template>
-<style></style>
 
-export const H800p3 = base(Contain, { classOut: "min-h-[800px]", pad: "px-2
-md:px-[3%]", classIn: " flex ", }); export const H0p3 = base(Contain, {
-classOut: "", pad: "px-2 md:px-[%]", classIn: " flex ", }); export const
-Brilliant = base(Contain, { classOut: "", pad: "px-2 sm:px-[15%] md:px-[20%]
-lg:px-[22%] xl:px-[24%]", classIn: " flex flex-wrap ", }); export const ContainX
-= { H800p3, H0p3, Brilliant, };
+<!-- variations: minheight, padding, flex-wrap -->
+<!-- container cannot be  -->
+<!-- pad: add room for the container, not content inside  -->
+<!-- classIn: inner classes -->
+
+<style></style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,51 +1,27 @@
+<script setup>
+const gogo = {
+  classOut: "px-[30%]",
+  classIn: "flex p-10",
+};
+</script>
+
 <template>
   <div>
-    <Contain pad="">
+    <Contain :="gogo">
       <h1 class="text-6xl">oh hi Mark</h1>
       <p class="text-sm">
-        oh hi Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Dignissimos autem consequatur id enim culpa iure suscipit, nisi placeat
-        earum consectetur saepe possimus officia. Dolor ipsum magni dolorem
-        recusandae facilis eum eius temporibus dolores ut optio facere, totam
-        officiis laboriosam deserunt inventore reiciendis tenetur dignissimos
-        culpa unde dolorum, autem eligendi placeat voluptatem! Accusamus nemo
-        dolore et autem, possimus asperiores ipsum rem, corporis in ducimus fuga
-        maxime praesentium? Nesciunt ducimus et totam culpa qui, suscipit saepe
-        a at incidunt id eligendi praesentium sunt unde quam ullam dolores
-        tempore quasi labore placeat odio sint. Non voluptatem placeat, deleniti
-        nostrum, repellat officiis animi voluptatum officia, quam minima
-        eligendi atque nulla! Repellendus inventore impedit illum itaque cumque
-        perspiciatis, adipisci a, explicabo ipsum sed optio voluptates
-        asperiores quasi pariatur dolor quia nam eos, in id? Sequi facilis non
-        labore cumque animi aliquam neque deleniti, quod repellat laborum rem
-        nisi laudantium quibusdam voluptatibus molestias error expedita ex.
-        Atque aperiam nostrum necessitatibus consequuntur facere ex delectus
-        enim et quisquam ut modi cupiditate deleniti minus eos eum, eaque
-        adipisci veritatis debitis suscipit asperiores ipsa iusto voluptatibus
-        placeat? Magnam ab, explicabo beatae voluptatibus voluptate nisi soluta
-        maiores mollitia nesciunt at a doloremque quisquam porro qui unde labore
-        repudiandae consectetur amet harum ea facilis. Ipsum, beatae nam fuga
-        fugit voluptatibus quos reiciendis officia eius accusamus? Quibusdam
-        laboriosam officiis, totam repellat modi sint veniam, doloribus
-        doloremque at impedit ut enim, facilis voluptatibus quo. Doloremque modi
-        libero iusto laborum minus. Facere non dolorem eveniet dolores optio
-        consectetur iusto error vero nostrum distinctio aliquid rem nemo
-        incidunt perspiciatis quaerat quod harum tempore similique explicabo
-        doloremque animi aut sit, aperiam dolore. Quis, optio odit et tenetur
-        veniam earum, voluptate eius fugit sunt, commodi aliquam rerum? Ullam
-        molestias harum amet. Nulla fugiat corrupti cupiditate eum molestiae
-        adipisci ex quae. Quas, ratione. Commodi repellendus dicta placeat
-        reiciendis fugit quis quod nihil earum qui officia delectus aperiam
-        atque laboriosam aspernatur minus consequuntur est maiores reprehenderit
-        odit, quaerat maxime incidunt cum. Minima quaerat odit animi.
-        Aspernatur, illum fugiat? Quod voluptatibus quis commodi minus odit?
-        Excepturi laborum esse error veniam ipsa iusto est dolorum earum
-        reiciendis cumque repellat, at necessitatibus eaque praesentium impedit
-        aliquam rerum sint. A nisi modi quaerat, magnam pariatur sint odio iste
-        tenetur doloremque voluptatem neque vitae at porro. Dolorem culpa odio
-        ut, doloremque commodi et ducimus quas impedit itaque ipsam delectus
-        quibusdam. Voluptatibus, molestiae ratione minus autem deserunt ut ullam
-        et aut sint porro impedit labore.
+        oh hi Lorem ipsum dolor sit, amet consectetur adi optio consectetur
+        iusto error vero nostrum distinctio aliquid rem nemo incidunt
+        perspiciatis quaerat quod harum tempore similique explicabo doloremque
+        animi aut sit, aperiam dolore. Quis, optio odit et tenetur veniam earum,
+        voluptate eius fugit sunt, commodi aliquam rerum? Ullam molestias harum
+        amet. Nulla fugiat corrupti cupiditate eum molestiae adipisci ex quae.
+        Quas, ratione. Commodi repellendus dicta placeat reiciendis fugit quis
+        quod nihil earum qui officipariatur sint odio iste tenetur doloremque
+        voluptatem neque vitae at porro. Dolorem culpa odio ut, doloremque
+        commodi et ducimus quas impedit itaque ipsam delectus quibusdam.
+        Voluptatibus, molestiae ratione minus autem deserunt ut ullam et aut
+        sint porro impedit labore.
       </p>
       <Button>hihih</Button>
       <NuxtLink to="examples/landing"> Nuxt website++ </NuxtLink>
